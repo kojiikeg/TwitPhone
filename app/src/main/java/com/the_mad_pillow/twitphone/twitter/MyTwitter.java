ce0725da7b76cbfb4829a8d785588ad8e7333a3b
themadpillow <mouhanpatumakura@gmail.com>
2018-09-26 16:20:21 +0900

    Userを先に取得しそこからデータを取得するよう修正 Follow等の取得はThread処理が必要の模様

--- app/src/main/java/com/the_mad_pillow/twitphone/twitter/MyTwitter.java
+++ app/src/main/java/com/the_mad_pillow/twitphone/twitter/MyTwitter.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Handler;
-import android.os.Message;
 import android.widget.Toast;
 
 import com.the_mad_pillow.twitphone.R;
@@ -19,93 +18,43 @@ import twitter4j.TwitterException;
 import twitter4j.User;
 
 public class MyTwitter {
-    //screenName, profileImage, follow, follower
-    private static final int TASK_COUNT = 4;
-    private Handler handler;
     private Context activity;
+    private Handler handler;
     private Twitter twitter;
-    private String screenName;
-    private String profileImageBigger;
-    private String profileImage400;
+    private User user;
     private List<MyUser> FFList;
     private List<User> followList;
     private List<User> followerList;
 
     @SuppressLint("HandlerLeak")
-    public MyTwitter(Context context, final Handler mainHandler) {
+    public MyTwitter(Context context, final Handler handler) {
         activity = context;
-        handler = new Handler() {
-            int count = 0;
-
-            @Override
-            public void handleMessage(Message msg) {
-                if (++count == TASK_COUNT) {
-                    mainHandler.sendEmptyMessage(activity.getResources().getInteger(R.integer.TwitterTask));
-                }
-            }
-        };
-
+        this.handler = handler;
         twitter = TwitterUtils.getTwitterInstance(context);
 
-        //ScreenNameの取得
-        //取得後フォロー・フォロワーの取得を開始する
-        initGetScreenName();
-        //ProfileImageの取得
-        initGetProfileImage();
-    }
-
-    public String getScreenName() {
-        return screenName;
-    }
-
-    public Twitter getTwitter() {
-        return twitter;
+        //Userの取得
+        initGetUser();
     }
 
     @SuppressLint("StaticFieldLeak")
-    private void initGetScreenName() {
+    private void initGetUser() {
         new AsyncTask<Void, Void, Void>() {
             @Override
             protected Void doInBackground(Void... voids) {
                 try {
-                    screenName = twitter.getScreenName();
+                    user = twitter.verifyCredentials();
                     return null;
                 } catch (TwitterException e) {
-                    e.printStackTrace();
-                    throw new RuntimeException("ScreenNameの取得に失敗");
+                    throw new RuntimeException("Userの取得に失敗");
                 }
             }
 
             @Override
             protected void onPostExecute(Void result) {
-                //フォローリストの取得
+                handler.sendEmptyMessage(activity.getResources().getInteger(R.integer.getUserTask));
+
                 initGetFollow();
-                //フォロワーリストの取得
                 initGetFollower();
-
-                handler.sendEmptyMessage(0);
-            }
-        }.execute();
-    }
-
-    @SuppressLint("StaticFieldLeak")
-    private void initGetProfileImage() {
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... voids) {
-                try {
-                    profileImageBigger = twitter.verifyCredentials().getBiggerProfileImageURLHttps();
-                    profileImage400 = twitter.verifyCredentials().get400x400ProfileImageURLHttps();
-                    return null;
-                } catch (TwitterException e) {
-                    e.printStackTrace();
-                    throw new RuntimeException("Profile画像取得に失敗");
-                }
-            }
-
-            @Override
-            protected void onPostExecute(Void result) {
-                handler.sendEmptyMessage(0);
             }
         }.execute();
     }
@@ -120,7 +69,7 @@ public class MyTwitter {
                     long cursor = -1;
                     int count = 0;
                     while (cursor != 0 && count < 15) {
-                        PagableResponseList<User> pagableResponseList = twitter.getFriendsList(screenName, cursor, 200);
+                        PagableResponseList<User> pagableResponseList = twitter.getFriendsList(user.getScreenName(), cursor, 200);
                         followList.addAll(pagableResponseList);
 
                         cursor = pagableResponseList.getNextCursor();
@@ -136,7 +85,7 @@ public class MyTwitter {
 
             @Override
             protected void onPostExecute(Void result) {
-                handler.sendEmptyMessage(0);
+                handler.sendEmptyMessage(activity.getResources().getInteger(R.integer.getListTask));
             }
         }.execute();
     }
@@ -151,7 +100,7 @@ public class MyTwitter {
                     long cursor = -1;
                     int count = 0;
                     while (cursor != 0 && count < 15) {
-                        PagableResponseList<User> pagableResponseList = twitter.getFollowersList(screenName, cursor, 200);
+                        PagableResponseList<User> pagableResponseList = twitter.getFollowersList(user.getScreenName(), cursor, 200);
                         followerList.addAll(pagableResponseList);
 
                         cursor = pagableResponseList.getNextCursor();
@@ -167,7 +116,7 @@ public class MyTwitter {
 
             @Override
             protected void onPostExecute(Void result) {
-                handler.sendEmptyMessage(0);
+                handler.sendEmptyMessage(activity.getResources().getInteger(R.integer.getListTask));
             }
         }.execute();
     }
@@ -185,14 +134,6 @@ public class MyTwitter {
                 ((Activity) activity).finish();
             }
         });
-    }
-
-    public String getProfileImageBigger() {
-        return profileImageBigger;
-    }
-
-    public String getProfileImage400() {
-        return profileImage400;
     }
 
     public List<User> getFollowList() {
@@ -233,5 +174,9 @@ public class MyTwitter {
         }
 
         return list;
+    }
+
+    public User getUser() {
+        return user;
     }
 }
